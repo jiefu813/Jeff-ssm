22f52c64ecd5f0c6d9b4d146b9ab8b79c289cc37
jiefu813 <jiefu813@163.com>
2019-06-04 00:00:09 +0800

    使用ehcache缓存授权信息，当授权信息改变时，需要清理缓存。

--- src/main/java/com/jeff/common/shiro/UserRealm.java
+++ src/main/java/com/jeff/common/shiro/UserRealm.java
@@ -3,6 +3,7 @@ package com.jeff.common.shiro;
 import com.jeff.entity.User;
 import com.jeff.service.MenuService;
 import com.jeff.service.UserService;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -11,9 +12,9 @@ import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.ByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.List;
 
 /**
@@ -30,6 +31,7 @@ public class UserRealm extends AuthorizingRealm {
     //授权
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        System.out.println("======授权======");
         // 获取身份信息--该身份信息在认证时已设置
         User user = (User) principals.getPrimaryPrincipal();
         // 根据身份信息获取权限数据
@@ -50,11 +52,19 @@ public class UserRealm extends AuthorizingRealm {
     //认证
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        System.out.println("======认证======");
         // 从token 中获取身份信息
         String username = token.getPrincipal().toString();
         // 根据用户名到数据库中取出用户信息如果查询不到返回null
         User user = userService.selectUserByLoginName(username);
         // 返回认证信息
         return new SimpleAuthenticationInfo(user, user.getPassword(), ByteSource.Util.bytes(user.getSalt()), this.getName());
+    }
+
+    //清理缓存方法
+    protected void clearCache() {
+        System.out.println("======清理缓存======");
+        Subject subject = SecurityUtils.getSubject();
+        super.clearCache(subject.getPrincipals());
     }
 }
